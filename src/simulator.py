6badd61357bf28d6f1cc646c062dd82473eb3132
v-argunov <argunovvlad5@gmail.com>
2022-10-16 01:33:07 +0300

    Added pep8 styles plus some corrections

--- src/simulator.py
+++ src/simulator.py
@@ -1,24 +1,31 @@
-import os
+"""
+Simulator deals with executing the agents
+strategy and logging values
+"""
+
+from typing import Optional
 
 import numpy as np
-import pandas as pd
 
-from typing import Optional, Union
+from tqdm import tqdm
+import wandb
 
 from src.datamodule import DataModule
-from tqdm import tqdm
-
-import wandb
 
 
 class Simulator():
+    """
+    Simulator handles all the execution of any strategy
+    inherited from base_strategy
+    """
     def __init__(self, train_test_split_time : str='2013-01-02',
                  use_wandb : bool=True, debug_mode : bool=False,
                  run_name : Optional[str]='SampleStrategy1'):
-        self.dm = DataModule()
-        self.dm.setup()
+        self.datamodule = DataModule()
+        self.datamodule.setup()
 
-        self.use_wandb = use_wandb
+        # if self.debug_mode = True, then do not log any values
+        self.use_wandb = use_wandb and not debug_mode
         self.debug_mode = debug_mode
 
         self.train_test_split_time = train_test_split_time
@@ -30,55 +37,74 @@ class Simulator():
 
         self.test_data_returns = None
 
+        self.value_portfolio_cache = [1,]
+
         self._configure_simulator()
         self._configure_wandb()
 
 
     def _configure_simulator(self):
-        self.train_data = self.dm.get_data(start_date=-1,
+        """
+        Configure simulator by splitting the
+        data according to train_test_split_time
+        """
+        self.train_data = self.datamodule.get_data(start_date=-1,
                                            end_date=self.train_test_split_time)
 
-        self.test_data = self.dm.get_data(start_date=self.train_test_split_time,
+        self.test_data = self.datamodule.get_data(start_date=self.train_test_split_time,
                                           end_date=-1)
         self.test_data_returns = self.test_data.pct_change()
 
 
     def _configure_wandb(self):
-        if (not self.use_wandb) or (self.debug_mode):
-            os.environ['WANDB_MODE'] = 'disabled'
+        """
+        Configure wandb logging
+        """
+        if not self.use_wandb:
+            self.wandb_run = wandb.init(reinit=True, name=self.run_name,
+                                        entity="cmf-credit-derivatives",
+                                        project='CMF-Credit-Derivatives')
 
-        self.wandb_run = wandb.init(reinit=True, name=self.run_name,
-                                    project='CMF-Credit-Derivatives')
-
-    def simulate(self, strategy, verbose=True):
+    def simulate(self, strategy, verbose: bool=True):
+        """
+        Simulates the strategy according the predefined
+        configuration
+        """
 
         value_portfolio = 1
         # Firstly train the strategy
         strategy.train_model(train_data=self.train_data)
 
         # Then test it
-        previous_prices = 1
         for idx in tqdm(range(self.test_data.shape[0])):
             current_prices = self.test_data.iloc[idx]
 
             if idx != 0:
-                current_return = 1 + self._update_value(portfolio, idx)
-                wandb.log({'return_portfolio' : current_return})
-                self.value_portfolio *= (1 + current_return)
+                current_return = self._update_value(portfolio, idx)
+                value_portfolio *= (1 + current_return)
+                self.value_portfolio_cache.append(value_portfolio)
+
+                if not self.use_wandb:
+                    wandb.log({'daily_return' : current_return})
+                    wandb.log({'portfolio_value' : value_portfolio})
                 if self.debug_mode:
                     break
 
             # Create a portfolio for next step
-            portfolio = strategy.trade(daily_data=prices.to_dict())
+            portfolio = strategy.trade(daily_data=current_prices.to_dict())
 
-        # After loop completion, report the results
-        wandb.log({'value_portfolio' : value_portfolio})
+        final_metrics = self._calculate_final_metrics()
 
         if verbose:
             print(f'Final value of portfolio {value_portfolio}')
+            print(f"Sharpe of the porfolio {final_metrics['sharpe']}")
 
-    def _update_value(self, current_portfolio : dict, idx : int):
-        total_portfolio = sum([abs(weight) for weight in current_portfolio.value()])
+    def _update_value(self, current_portfolio : dict, idx : int) -> float:
+        """
+        Update the value of the portfolio according to the
+        weights of the portfolio
+        """
+        total_portfolio = sum([abs(weight) for weight in current_portfolio.values()])
         assert total_portfolio <= 1, 'You have exceeded overall allocations! \
                                       All weights must sum to 1, including the negative allocations!'
 
@@ -86,3 +112,19 @@ class Simulator():
         current_returns = self.test_data_returns.iloc[idx]
         return sum([current_portfolio[asset] * current_returns[asset] \
                                 for asset in current_portfolio.keys()])
+
+    def _calculate_final_metrics(self) -> dict:
+        """
+        Calculate the final metrics for the strategy
+        reporting
+        Metrics calculated:
+        Sharpe
+        """
+        sharpe = np.array(self.value_portfolio_cache).mean() \
+                          / np.array(self.value_portfolio_cache).std()
+
+        final_metrics = {'sharpe' : sharpe}
+        if not self.use_wandb:
+            wandb.log(final_metrics)
+
+        return final_metrics
