c2d40681a6d9b86693a34d0ad7938386890ad11f
Vladislav Argunov <101595918+vladargunov@users.noreply.github.com>
2022-10-30 20:03:28 +0300

    again update correct Wandb project name

--- src/simulator.py
+++ src/simulator.py
@@ -97,7 +97,7 @@ class Simulator():
         Configure wandb logging
         """
         if self.use_wandb:
-            project_name = self.project_name + '_v1.2'
+            project_name = self.project_name + '_v1-2'
             self.wandb_run = wandb.init(reinit=True, name=self.run_name,
                                         entity="cmf-credit-derivatives",
                                         project=project_name)
